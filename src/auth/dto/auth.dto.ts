4a9bf170f597c3b721c5be0ab2556fa3f47db9fb
Nilesh <nilesh.jaiswar006@gmail.com>
2025-10-23 22:20:37 +0530

    Implement user authentication service and DTOs

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,29 @@
+import { IsEmail, IsString, MinLength, IsNotEmpty } from 'class-validator';
+
+export class SignupDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
+
+export class LoginDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class RefreshTokenDto {
+  @IsString()
+  @IsNotEmpty()
+  refreshToken: string;
+}
